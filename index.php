c23d104521834cdebbe3dcb704ce342c9dc0fa54
Group1 <hongpink473@gmail.com>
2025-01-19 20:50:23 +0700

    Create/Write

--- index.php
+++ index.php
@@ -9,12 +9,20 @@
 <body>
     <?php
 
-      $myfile = fopen("./file.txt", "r") or die("File not found");
-      while(!feof($myfile)){
-        echo fgets($myfile) ." <br/>";
-      }
+      $myfile = fopen("./file.txt", "a") or die("File not found");
+      $txt = "Chheng Han\n";
+      fwrite($myfile, $txt);
+      $txt = "Srey Nich\n";
+      fwrite($myfile, $txt);
+      $txt = "Mey\n";
+      fwrite($myfile, $txt);
       fclose($myfile);
 
+      $myfile = fopen("./file.txt", "r") or die("File not found");
+      while(!feof($myfile)){
+        echo fgets($myfile) . "<br/>";
+      }
+      fclose($myfile);
     ?>
 </body>
 </html>
